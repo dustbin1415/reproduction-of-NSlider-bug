2c4e9292c5426e9e242386e49a07acf5e7cd2b13
dustbin <53084044+dustbin1415@users.noreply.github.com>
2024-07-07 02:06:28 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { NSlider } from 'naive-ui'
+</script>
+
+<template>
+  <main>
+    <div style="height: 300px;">
+      <NSlider vertical />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
